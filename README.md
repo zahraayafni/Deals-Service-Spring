8f6bf35f297ab063447a2bf506f6f3f28d89addd
Zahrah A A Febriani <zahrahfebriani@gmail.com>
2019-05-11 21:42:35 +0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Deals-Service-Spring
 A repository for Framework Programming Final Project
+
+## API Information
+
+| Endpoint  | Method | Function |
+| ------------- | ------------- | ------------- |
+| /deals | GET | Show all deals |
+| /deals/active | GET | Show all active deals |
+| /deals/exp | GET | Show all expired deals |
+| /deals/{r_id} | GET | Show all deals by restaurant id |
+| /deals/{r_id}/active | GET | Show all active deals by restaurant id |
+| /deals/{r_id}/exp | GET  | Show all expired deals by restaurant id |
+| /deals/{r_id} | POST  | Create deals |
+| /deals/{r_id}/{id} | GET | Show deals by id for a restaurant |
+| /deals/{r_id}/{id} | PUT | Update a deals by id for a restaurant |
+| /deals/{r_id}/{id} | DELETE | Delete a deals by id for a restaurant |
+| /deals/use/{u_id}/{id} | POST | Check for voucher permission |
+| /deals/{u_id}/{id} | POST | Use voucher by a user |
+| /deals/{u_id} | GET | Show voucher history of a user |
--- pbkkd-deals-service/src/main/java/id/ac/its/model/Deals.java
+++ pbkkd-deals-service/src/main/java/id/ac/its/model/Deals.java
@@ -4,45 +4,46 @@ import java.util.Date;
 
 public class Deals {
 
-	Integer d_id;
-	String d_code;
-	String d_name;
-	String d_desc;
-	Integer d_type; // percent is 0 and money is 1
-	Double d_amount;
-	Integer d_max_val; // given the maximum value for redeemed voucher
-	Integer d_min_val; // given the minimum value for transaction/using voucher
-	Integer d_limit_use;
-	Boolean d_limit_one_cust;
-	Boolean d_new_cust_only;
-	Date d_start;
-	Date d_end;
+	Integer id;
+	String code;
+	String name;
+	String desc;
+	Integer type; // percent is 0 and money is 1
+	Double amount;
+	Integer max_val; // given the maximum value for redeemed voucher
+	Integer min_val; // given the minimum value for transaction/using voucher
+	Integer total_limit_use;
+	Integer limit_use_per_user; // how many time the voucher can be redeemed by user
+	Boolean limit_one_cust_only; // each user only have one chance to redeem the voucher
+	Boolean new_cust_only;
+	Date start;
+	Date end;
 	Date create_at;
 	Date update_at;
 
 	public Deals(Integer d_id, String d_name) {
 		super();
-		this.d_id = d_id;
-		this.d_name = d_name;
+		this.id = d_id;
+		this.name = d_name;
 	}
 
 	public Deals(Integer d_id, String d_code, String d_name, String d_desc, Integer d_type, Double d_amount, Integer d_max_val,
 			Integer d_min_val, Integer d_limit_use, Boolean d_limit_one_cust, Boolean d_new_cust_only, Date d_start,
 			Date d_end, Date create_at, Date update_at) {
 		super();
-		this.d_id = d_id;
-		this.d_code = d_code;
-		this.d_name = d_name;
-		this.d_desc = d_desc;
-		this.d_type = d_type;
-		this.d_amount = d_amount;
-		this.d_max_val = d_max_val;
-		this.d_min_val = d_min_val;
-		this.d_limit_use = d_limit_use;
-		this.d_limit_one_cust = d_limit_one_cust;
-		this.d_new_cust_only = d_new_cust_only;
-		this.d_start = d_start;
-		this.d_end = d_end;
+		this.id = d_id;
+		this.code = d_code;
+		this.name = d_name;
+		this.desc = d_desc;
+		this.type = d_type;
+		this.amount = d_amount;
+		this.max_val = d_max_val;
+		this.min_val = d_min_val;
+		this.total_limit_use = d_limit_use;
+		this.limit_one_cust_only = d_limit_one_cust;
+		this.new_cust_only = d_new_cust_only;
+		this.start = d_start;
+		this.end = d_end;
 		this.create_at = create_at;
 		this.update_at = update_at;
 	}
@@ -50,123 +51,123 @@ public class Deals {
 	public void update(String d_code, String d_name, String d_desc, Integer d_type, Double d_amount, Integer d_max_val,
 			Integer d_min_val, Integer d_limit_use, Boolean d_limit_one_cust, Boolean d_new_cust_only, Date d_start,
 			Date d_end, Date update_at) {
-		this.d_code = d_code;
-		this.d_name = d_name;
-		this.d_desc = d_desc;
-		this.d_type = d_type;
-		this.d_amount = d_amount;
-		this.d_max_val = d_max_val;
-		this.d_min_val = d_min_val;
-		this.d_limit_use = d_limit_use;
-		this.d_limit_one_cust = d_limit_one_cust;
-		this.d_new_cust_only = d_new_cust_only;
-		this.d_start = d_start;
-		this.d_end = d_end;
+		this.code = d_code;
+		this.name = d_name;
+		this.desc = d_desc;
+		this.type = d_type;
+		this.amount = d_amount;
+		this.max_val = d_max_val;
+		this.min_val = d_min_val;
+		this.total_limit_use = d_limit_use;
+		this.limit_one_cust_only = d_limit_one_cust;
+		this.new_cust_only = d_new_cust_only;
+		this.start = d_start;
+		this.end = d_end;
 		this.update_at = update_at;
 	}
 
 	public Integer getD_id() {
-		return d_id;
+		return id;
 	}
 
 	public void setD_id(Integer d_id) {
-		this.d_id = d_id;
+		this.id = d_id;
 	}
 
 	public String getD_code() {
-		return d_code;
+		return code;
 	}
 
 	public void setD_code(String d_code) {
-		this.d_code = d_code;
+		this.code = d_code;
 	}
 
 	public String getD_name() {
-		return d_name;
+		return name;
 	}
 
 	public void setD_name(String d_name) {
-		this.d_name = d_name;
+		this.name = d_name;
 	}
 
 	public String getD_desc() {
-		return d_desc;
+		return desc;
 	}
 
 	public void setD_desc(String d_desc) {
-		this.d_desc = d_desc;
+		this.desc = d_desc;
 	}
 
 	public Integer getD_type() {
-		return d_type;
+		return type;
 	}
 
 	public void setD_type(Integer d_type) {
-		this.d_type = d_type;
+		this.type = d_type;
 	}
 
 	public Double getD_amount() {
-		return d_amount;
+		return amount;
 	}
 
 	public void setD_amount(Double d_amount) {
-		this.d_amount = d_amount;
+		this.amount = d_amount;
 	}
 
 	public Integer getD_max_val() {
-		return d_max_val;
+		return max_val;
 	}
 
 	public void setD_max_val(Integer d_max_val) {
-		this.d_max_val = d_max_val;
+		this.max_val = d_max_val;
 	}
 
 	public Integer getD_min_val() {
-		return d_min_val;
+		return min_val;
 	}
 
 	public void setD_min_val(Integer d_min_val) {
-		this.d_min_val = d_min_val;
+		this.min_val = d_min_val;
 	}
 
 	public Integer getD_limit_use() {
-		return d_limit_use;
+		return total_limit_use;
 	}
 
 	public void setD_limit_use(Integer d_limit_use) {
-		this.d_limit_use = d_limit_use;
+		this.total_limit_use = d_limit_use;
 	}
 
 	public Boolean getD_limit_one_cust() {
-		return d_limit_one_cust;
+		return limit_one_cust_only;
 	}
 
 	public void setD_limit_one_cust(Boolean d_limit_one_cust) {
-		this.d_limit_one_cust = d_limit_one_cust;
+		this.limit_one_cust_only = d_limit_one_cust;
 	}
 
 	public Boolean getD_new_cust_only() {
-		return d_new_cust_only;
+		return new_cust_only;
 	}
 
 	public void setD_new_cust_only(Boolean d_new_cust_only) {
-		this.d_new_cust_only = d_new_cust_only;
+		this.new_cust_only = d_new_cust_only;
 	}
 
 	public Date getD_start() {
-		return d_start;
+		return start;
 	}
 
 	public void setD_start(Date d_start) {
-		this.d_start = d_start;
+		this.start = d_start;
 	}
 
 	public Date getD_end() {
-		return d_end;
+		return end;
 	}
 
 	public void setD_end(Date d_end) {
-		this.d_end = d_end;
+		this.end = d_end;
 	}
 
 	public Date getCreate_at() {
@@ -187,10 +188,10 @@ public class Deals {
 
 	@Override
 	public String toString() {
-		return "Deals [d_id=" + d_id + ", d_code=" + d_code + ", d_name=" + d_name + ", d_desc=" + d_desc + ", d_type="
-				+ d_type + ", d_amount=" + d_amount + ", d_max_val=" + d_max_val + ", d_min_val=" + d_min_val
-				+ ", d_limit_use=" + d_limit_use + ", d_limit_one_cust=" + d_limit_one_cust + ", d_new_cust_only="
-				+ d_new_cust_only + ", d_start=" + d_start + ", d_end=" + d_end + ", create_at=" + create_at
+		return "Deals [d_id=" + id + ", d_code=" + code + ", d_name=" + name + ", d_desc=" + desc + ", d_type="
+				+ type + ", d_amount=" + amount + ", d_max_val=" + max_val + ", d_min_val=" + min_val
+				+ ", d_limit_use=" + total_limit_use + ", d_limit_one_cust=" + limit_one_cust_only + ", d_new_cust_only="
+				+ new_cust_only + ", d_start=" + start + ", d_end=" + end + ", create_at=" + create_at
 				+ ", update_at=" + update_at + "]";
 	}
 	
