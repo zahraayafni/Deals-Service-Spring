dc498d9e78f27a2682a98887b974e3b74b9e08b6
fadillasa <fadillasukmaal@gmail.com>
2019-05-20 11:17:46 +0700

    Update form_add_deals.php

--- web-interface/form_add_deals.php
+++ web-interface/form_add_deals.php
@@ -71,7 +71,7 @@
 <html>
 <head>
 	<title>Add Deals</title>
-	<style type="text/css">
+	<!-- <style type="text/css">
 		.deals-form {
 			margin-left: 35%;
 		}
@@ -79,90 +79,161 @@
 			margin-top: 5%;
 			margin-left: 38%;
 		}
-	</style>
+	</style> -->
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="assets/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="assets/plugins/iCheck/square/blue.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body>
-	<div class="deals-form">
-		<h1>Add Deals</h1>
 
-		<form action="" method="POST">
-			<table>
-				<tr>
-					<td>Kode voucher</td>
-					<td><input type="text" name="code" required></td>
-				</tr>
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+        <header class="main-header">
+            <a href="#" class="logo" data-toggle="push-menu" role="button" style="text-decoration: none">
+              <span class="logo-mini"><img src=""></span>
+              <span class="logo-lg"><b>Deals </b>Service</span>
+              <span class="sr-only">Toggle navigation</span>
+            </a>
+        </header>
+        <aside class="main-sidebar">    
+            <section class="sidebar">        
+              <div class="user-panel">
+                <div class="pull-left image">
+                  <img src="assets/dist/img/admin.png" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                  <p>User</p>
+                  <a><i class="fa fa-circle text-success"></i> Online</a>
+                </div>
+             </div>
+             <ul class="sidebar-menu" data-widget="tree">
+              <li><?php echo '<a href="get_all_deals.php?r_id='.$r_id.'" style="text-decoration: none"><i class="fa fa-circle-o"></i> All</a>' ?></li>
+              <li><?php echo '<a href="get_all_active_deals.php?r_id='.$r_id.'" style="text-decoration: none"><i class="fa fa-circle-o"></i> Active</a>' ?></li>
+              <li><?php echo '<a href="get_all_expired_deals.php?r_id='.$r_id.'" style="text-decoration: none"><i class="fa fa-circle-o"></i> Expired</a>' ?></li>
+              <li><?php echo '<a href="form_add_deals.php?r_id='.$r_id.'" style="text-decoration: none"><i class="fa fa-book"></i> Add Deals</a>' ?></li>
+              <li>
+                <a href="index.php" style="text-decoration: none">
+                  <i class="fa fa-sign-out"></i> <span>Logout</span>
+                </a>
+              </li>
+             </ul>
+           </section>
+           <!-- /.sidebar -->
+        </aside>
 
-				<tr>
-					<td>Nama voucher</td>
-					<td><input type="text" name="name" required></td>
-				</tr>
+        <div class="content-wrapper">
+            <section class="content-header">
+                <h1>
+                    Add Deals
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+                    <li class="active">Add Deals</li>
+                </ol>
+            </section>           
+            <section class="content">
+            	<div class="box box-primary">
+            		<form action="" method="post" enctype="multipart/form-data" role="form">
+            			<div class="box-body">
+            				<div class="form-group">
+            					<label>Kode Voucher</label>
+            					<input type="text" class="form-control" name=code placeholder="Masukkan kode voucher" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Nama Voucher</label>
+            					<input type="text" class="form-control" name=name placeholder="Masukkan nama voucher" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Deskripsi</label>
+            					<input type="text" class="form-control" name=description placeholder="Masukkan deskripsi" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Tipe Voucher</label>
+            					<br>
+            					<select name="type" required>
+            						<option value="0">Presentase</option>
+            						<option value="1">Potongan langsung</option>
+            					</select>
+                          	</div>
+            				<div class="form-group">
+            					<label>Besar Diskon</label>
+            					<input type="text" class="form-control" name=amount placeholder="Masukkan besar diskon" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Max Diskon</label>
+            					<input type="text" class="form-control" name=max_val placeholder="Masukkan maksimal diskon" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Min Belanja</label>
+            					<input type="text" class="form-control" name=min_val placeholder="Masukkan minimal belanja" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Banyak Voucher</label>
+            					<input type="text" class="form-control" name=total_limit_use placeholder="Masukkan banyak voucher" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Batas penggunaan tiap customer</label>
+            					<input type="text" class="form-control" name=limit_use_per_user placeholder="Masukkan batas penggunaan tiap customer" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Hanya Customer baru</label><br>
+            					<input type="radio" name="new_cust_only" value="true" required> Ya
+            					<br>
+            					<input type="radio" name="new_cust_only" value="false" required> Tidak
+            				</div>
+            				<div class="form-group">
+            					<label>Status Voucher</label><br>
+            					<input type="radio" name="active_status" value="true" required> Aktifkan
+            					<br>
+            					<input type="radio" name="active_status" value="false" required> Nanti Dulu
+            				</div>
+							<div class="form-group">
+            					<label>Berlaku sejak</label>
+            					<input type="text" class="form-control" name=start placeholder="Masukkan waktu mulai berlaku" required>
+            				</div>
+            				<div class="form-group">
+            					<label>Berakhir pada</label>
+            					<input type="text" class="form-control" name=end_time placeholder="Masukkan waktu berakhir" required>
+            				</div>
+            			</div>
+            			<div class="box-footer">
+            				<button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+            				<button type="reset" class="btn btn-md btn-danger">Reset</button>
+            			</div>
+            		</form>
+            	</div>
+            </section>
+        </div>
+    </div>
 
-				<tr>
-					<td>Deskripsi</td>
-					<td><input type="text" name="description" required></td>
-				</tr>
+    <script>
+        $.widget.bridge('uibutton', $.ui.button);
+    </script>
+    <script src="assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
-				<tr>
-					<td>Tipe voucher</td>
-					<td>
-						<select name="type" required>
-							<option value="0">Presentase</option>
-							<option value="1">Potongan langsung</option>
-						</select>
-					</td>
-				</tr>
-
-				<tr>
-					<td>Besar diskon</td>
-					<td><input type="text" name="amount" required></td>
-				</tr>
-
-				<tr>
-					<td>Max diskon</td>
-					<td><input type="text" name="max_val" required></td>
-				</tr>
-
-				<tr>
-					<td>Min belanja</td>
-					<td><input type="text" name="min_val" required></td>
-				</tr>
-
-				<tr>
-					<td>Banyak voucher</td>
-					<td><input type="text" name="total_limit_use" required></td>
-				</tr>
-
-				<tr>
-					<td>Batas penggunaan tiap customer</td>
-					<td><input type="text" name="limit_use_per_user" required></td>
-				</tr>
-
-				<tr>
-					<td>Hanya customer baru</td>
-					<td><input type="radio" name="new_cust_only" value="true" required> Ya
-					<input type="radio" name="new_cust_only" value="false" required> Tidak </td>
-				</tr>
-
-				<tr>
-					<td>Status voucher</td>
-					<td><input type="radio" name="active_status" value="true" required> Aktifkan
-						<input type="radio" name="active_status" value="false" required> Nanti Dulu </td>
-				</tr>
-
-				<tr>
-					<td>Berlaku sejak</td>
-					<td><input type="text" name="start" required></td>
-				</tr>
-
-				<tr>
-					<td>Berakhir pada</td>
-					<td><input type="text" name="end_time" required></td>
-				</tr>
-			</table>
-
-			<input class="submit" type="submit" name="submit" value="Submit">
-		</form>
-	</div>
+    <script>
+        $(function () {
+            $('#example1').DataTable()
+            $('#example2').DataTable({
+                'paging': true,
+                'lengthChange': false,
+                'searching': false,
+                'ordering': true,
+                'info': true,
+                'autoWidth': false
+            })
+        })
+    </script>
 	
 </body>
 </html>
